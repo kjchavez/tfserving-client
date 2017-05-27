c9f811c3555592d477b34cd4dba41ee8b65abfb1
Kevin Chavez <kevin.j.chavez@gmail.com>
2017-05-27 13:01:42 -0700

    renamed

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='python_predict_client',
+setup(name='tfserving_client',
       version='0.1',
       description='Standalone python client for TF serving',
-      url='http://github.com/kjchavez/python-predict-client',
+      url='http://github.com/kjchavez/tfserving-client',
       author='tobegit3hub, extracted by kjchavez',
       author_email='kevin.j.chavez@gmail.com',
       license='MIT',
-      packages=['python_predict_client'],
+      packages=['tfserving_client'],
       zip_safe=False)
